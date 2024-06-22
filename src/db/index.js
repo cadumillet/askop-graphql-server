022b897f57e269bc2ec774f8be6a415e0b3425b3
Carlos Millet <cadumillet@icloud.com>
2024-06-22 19:48:58 -0300

    remove fake db

--- src/db/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-const lowdb_1 = require("lowdb");
-const node_1 = require("lowdb/node");
-exports.db = new lowdb_1.Low(new node_1.JSONFile("database.json"), null);
